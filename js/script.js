818a29b42900783cb257dcb61a0bb9f21d01ea64
Thoriq Firdaus <tfirdaus@outlook.com>
2015-12-18 22:12:45 +0700

    update debounce

--- js/script.js
+++ js/script.js
@@ -1,78 +1,105 @@
+(function( $ ){
+
+	var debounce = function( func, threshold, execAsap ) {
+
+		var timeout;
+
+		return function debounced() {
+			var obj = this, args = arguments;
+			function delayed () {
+				if (!execAsap) {
+					func.apply(obj, args);
+				}
+				timeout = null;
+			}
+
+			if (timeout) {
+				clearTimeout(timeout);
+			} else if (execAsap) {
+				func.apply(obj, args);
+			}
+
+			timeout = setTimeout(delayed, threshold || 100);
+		};
+	};
+
+	$.fn.deb = function( evt, func, threshold ) {
+		return func ? this.on( evt || 'resize', debounce( func, threshold ) ) : false;
+	};
+
+	$.fn.lazy = function( image ) {
+
+		var	self  = this;
+			image = $( image );
+
+		if ( self.is( '.is-visible' ) ) {
+			return;
+		}		
+
+		if ( self.visible( true ) ) {
+			
+			self.addClass( 'is-visible' );
+
+			var width  = image.data( 'large-width' );
+			var	height = image.data( 'large-height' );
+			var	imgNew = $( new Image( width, height ) );
+
+			imgNew
+				.attr({
+					'src'   : image.data( 'large' ),
+					'class' : 'img-large',
+					'alt'   : ''
+				})
+				.on( 'load', function() {
+					self.addClass( 'is-loaded' );
+					image.siblings( 'canvas' )
+						.after( $( this ).addClass( 'fade-in' ) );
+				});
+		}
+
+	};
+
+})( jQuery );
+
 (function( $ ) {
 
 	'use strict';
 
-	var acme = {
-		debounce : function( func, wait, immediate ) {
-			
-			var timeout;
-
-			return function() {
-				var context = this, args = arguments;
-				var later   = function() {
-					timeout = null;
-					if (!immediate) func.apply(context, args);
-				};
-				var callNow = immediate && !timeout;
-
-				clearTimeout(timeout);
-				timeout = setTimeout(later, wait);
-				if (callNow) func.apply(context, args);
-			};
-		},
-		img : function( img, canvas ) {
-
-			var width    = img.data( 'large-width' );
-			var	height   = img.data( 'large-height' );
-			var	imgLarge = $( new Image( width, height ) );
-
-			imgLarge
-			.attr({
-				'src'   : img.data( 'large' ),
-				'class' : 'img-large',
-				'alt'   : ''
-			})
-			.on( 'load', function() {
-				img.parent().addClass( 'is-loaded' );
-				canvas.after( $( this ).addClass( 'fade-in' ) );
-			});
-		}
-	};
-
 	$( '#content' )
 		.imagesLoaded()
-		.progress( function( instance, image ) {
+			.progress( function( instance, image ) {
 
-			if ( false === image.isLoaded ) {
-				return;
-			}
-			var img    = image.img;
-			var	canvas = image.img.nextElementSibling;
-
-			window.StackBlur.image( img, canvas, 6 );
-			canvas.classList.add( 'img-blur', 'fade-in' );
-
-		}).done( function( instance ) {
-
-			$.each( instance.images, function( index, image ) {
-
-				var	img    = $( image.img );
-				var	canvas = $( image.img.nextElementSibling );
-				var figure = $( image.img.parentElement );
-
-				if ( figure.visible( true ) ) {
-					figure.addClass( 'is-visible' );
-					acme.img( img, canvas );
-				} else {
-					$( window ).on( 'scroll', acme.debounce( function() {
-						if ( figure.visible( true ) && ! figure.is( '.is-visible' ) ) {
-							figure.addClass( 'is-visible' );
-							acme.img( img, canvas );
-						}
-					}, 300 ));
+				if ( false === image.isLoaded ) {
+					return;
 				}
+				var img    = image.img;
+				var	canvas = image.img.nextElementSibling;
 
+				window.StackBlur.image( img, canvas, 6 );
+				canvas.classList.add( 'img-blur', 'fade-in' );
+
+			})
+			.done( function( instance ) {
+
+				const images = instance.images;
+
+				$.each( images, function( index, image ) {
+
+					$( image.img )
+						.parent( 'figure ')
+						.lazy( image.img );
+				});
+
+				$( window ).deb( 'scroll', function() {
+
+					$.each( images, function( index, image ) {
+
+						$( image.img )
+							.parent( 'figure ')
+							.lazy( image.img );
+					});
+
+				}, 240 );
 			});
-		});
 
 })( jQuery );
